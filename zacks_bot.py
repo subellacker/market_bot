34d4a81e6b224e55b630f1236b5ae4f97607bab9
Samuel Ubellacker <subella@mit.edu>
2018-08-30 19:29:23 -0700

    added log files

--- zacks_bot.py
+++ zacks_bot.py
@@ -1,4 +1,5 @@
 import  urllib2
+import json
 import re
 import requests
 import datetime
@@ -20,13 +21,13 @@ class WebsiteScraper:
             soup = BeautifulSoup(c, "html.parser")
             return soup        
         except Exception as e:
-            print(e)
-            print("Failed to open URL")
+            f.write(e+"\n")
+            f.write("Failed to open URL\n")
             try:
-                print("Response Code: " +  str(response.status_code))
+                f.write("Response Code: " +  str(response.status_code)+ "\n")
             except Exception as e:
-                print(e)   
-                print("No response code")
+                f.write(e+"\n")   
+                f.write("No response code\n")
     
     def is_website_scraped(self, url):
         pass    
@@ -34,53 +35,56 @@ class WebsiteScraper:
     def return_stocks(self):
         return frozenset(self.stocks_set)
     
-    def print_stocks(self):
-        print ("Zacks recommends these stocks: "+ str(self.stocks_set))
-        print("----------------------------------------------------------------------------------------------------------")
+    def write_stocks(self):
+        f.write ("Zacks recommends these stocks: "+ str(self.stocks_set)+"\n")
+        f.write("----------------------------------------------------------------------------------------------------------\n")
 
 class ZacksScraper(WebsiteScraper):
     def __init__(self, base_url):
         WebsiteScraper.__init__(self, base_url)
         
     def is_website_scraped(self):
-        print("----------------------------------------------------------------------------------------------------------")
+        f.write("----------------------------------------------------------------------------------------------------------\n")
+        f.write("Timestamp: " + str(datetime.datetime.now()).split('.')[0]+"\n")
         is_base_url_verified = self.verify_url_0(self.base_url)
-        print ("Base url verified: " + str(is_base_url_verified)) 
-        print("------------------------------------")
+        f.write ("Base url verified: " + str(is_base_url_verified)+"\n") 
+        f.write("------------------------------------\n")
+        f.write("Timestamp: " + str(datetime.datetime.now()).split('.')[0]+"\n")
         is_top_url_verified =  self.verify_url_1(self.top_url)
-        print ("Top stocks verified: " + str(is_top_url_verified))                
-        print("------------------------------------")
+        f.write ("Top stocks verified: " + str(is_top_url_verified)+"\n")                
+        f.write("------------------------------------\n")
+        f.write("Timestamp: " + str(datetime.datetime.now()).split('.')[0]+"\n")
         verification_status = is_base_url_verified and is_top_url_verified 
-        print("Zacks verification status: " + str(verification_status))  
+        f.write("Zacks verification status: " + str(verification_status)+"\n")  
         return verification_status
 
     def verify_url_0(self, url):
         try:
-            print("Parsing Zacks Rank...")
-            print("Finding appropriate link in main page...")
+            f.write("Parsing Zacks Rank...\n")
+            f.write("Finding appropriate link in main page...\n")
             soup = self.open_url(url)
             for element in soup.findAll("article", {"class": "bull_of_the_day"}):
                 s = element.text.strip()    
                 if "Bull of the Day" in element.text.strip():
                     bull = str((s[s.find("(")+1:s.find(")")])) 
-                    print("Bull of the Day: " + bull)
+                    f.write("Bull of the Day: " + bull+"\n")
                     self.stocks_set.add(bull)    
                     break
 
             for element in soup.findAll("p"):
                 if "Zacks #1 Rank Additions" in element.text.strip():
                     self.top_url = "https://www.zacks.com" + str(element.find('a').get('href'))
-                    print("Text Verification: " + str(element.text.strip()))
-                    print("Link Verification: " + self.top_url)
+                    f.write("Text Verification: " + str(element.text.strip())+"\n")
+                    f.write("Link Verification: " + self.top_url+"\n")
                     return True
             return False
         except Exception as e:
-            print(e)
+            f.write(e)
             return False
                
     def verify_url_1(self, url):
         try:
-            print("Parsing article page...")
+            f.write("Parsing article page...\n")
             soup = self.open_url(url)    
             article_date = soup.find("time").text.strip()
             article_title = str(soup.title.string)
@@ -89,26 +93,28 @@ class ZacksScraper(WebsiteScraper):
             is_article_verified = is_date_valid and is_title_valid 
             {self.stocks_set.add(str(i.text.strip())) for i in soup.findAll("span", {"class": "hoverquote-symbol"})};
             
-            print("System date: " + str(self.current_date))
-            print("Article date: " + str(article_date))
-            print("Dates match: " + str(is_article_verified))
-            print("Article Title: " +str(soup.title.string))
-            print("Stocks recommeded from article: " + str(self.stocks_set))
+            f.write("System date: " + str(self.current_date)+"\n")
+            f.write("Article date: " + str(article_date)+"\n")
+            f.write("Dates match: " + str(is_article_verified)+"\n")
+            f.write("Article Title: " +str(soup.title.string)+"\n")
+            f.write("Stocks recommeded from article: " + str(self.stocks_set)+"\n")
             return is_article_verified
 
         except Exception as e:
-            print(e)
+            f.write(e)
             return False
     
 
 #Open url and parse stocks to buy
+file_string = "/home/subella/src/market/logs/{}.txt".format(datetime.datetime.now().strftime("%y-%m-%d"))
+f = open(file_string, "w+", 1)
 stocks_set = set() 
 zacks_scraper = ZacksScraper("https://www.zacks.com/stocks/zacks-rank")
 
 while True:
     if zacks_scraper.is_website_scraped():
         stocks_set.add(zacks_scraper.return_stocks())
-        zacks_scraper.print_stocks()
+        zacks_scraper.write_stocks()
         break
 
 #init stock dict
@@ -118,9 +124,9 @@ for i in stocks_set:
     for j in i:
         if j not in bought_stocks:
             bought_stocks[j] = Stock(j).get_price()
-print("Bought these stocks: ")
-print(bought_stocks)
-print("----------------------------------------------------------------------------------------------------------")
+f.write("Bought these stocks: \n")
+f.write(json.dumps(bought_stocks)+"\n")
+f.write("----------------------------------------------------------------------------------------------------------\n")
 
 
 stocks_remaining = True
@@ -129,16 +135,16 @@ while stocks_remaining:
     for stock in bought_stocks.keys():
         buy_price = bought_stocks[stock]
         current_price = Stock(stock).get_price()
-        #print(stock + ": " + "Buy price: " + str(buy_price) + " Current price: " + str(current_price))
+        #f.write(stock + ": " + "Buy price: " + str(buy_price) + " Current price: " + str(current_price))
         if (current_price > (buy_price*1.01)):
-            print("Selling " + stock + " Buy price: " + str(buy_price) + " Sell price: " +str(current_price))
+            f.write("Selling " + stock + " Buy price: " + str(buy_price) + " Sell price: " +str(current_price)+"\n")
             del bought_stocks[stock]
         elif (current_price < (buy_price*.98)):
-            print("Selling " + stock + " Buy price: " + str(buy_price) + " Sell price: " +str(current_price))
+            f.write("Selling " + stock + " Buy price: " + str(buy_price) + " Sell price: " +str(current_price)+"\n")
             del bought_stocks[stock]
         elif (current_price == buy_price):
             pass
-            #print("Selling " + stock + " Buy price: " + str(buy_price) + " Sell price: " +str(current_price))
+            #f.write("Selling " + stock + " Buy price: " + str(buy_price) + " Sell price: " +str(current_price))
             #del bought_stocks[stock]
     stocks_remaining = bool(bought_stocks) 
 
